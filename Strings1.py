a32e976fa227bf44ba1e1292cb6e167ab185eef3
Vulpeslnculta <98029041+Vulpeslnculta@users.noreply.github.com>
2022-01-19 19:59:24 +0100

    Add files via upload
    Guess I'll put this in my autostart

---
+++ Strings1.py
@@ -0,0 +1,9 @@
+print('Everyday is a good day to learn python')
+print('Python is fun :)')
+print("Python's strings are so easy to use")
+print('I can even include "quotes" in strings!')
+print("Hello" + "World")
+greeting = "Hello"
+name = input("Please enter your name <3 : ")
+
+print(greeting + ' ' + name)
