04602b6a0324ccd14ab3fa0c04a9cc0a8843218b
Vulpeslnculta <98029041+Vulpeslnculta@users.noreply.github.com>
2022-01-19 22:52:35 +0100

    Add files via upload
    Simple code containing essential info of main data types, and showing them off

---
+++ Numeric-data-types.py
@@ -0,0 +1,20 @@
+print('Sequence type of data is "String" (or "Str" for short) type, which look like this: \n')
+str = 'I walked a lonely road...'
+print(str)
+print(type(str))
+
+print('''\nAlso we have type of data named "Integer" or "int" for short. It stores numeric data in form of 
+natural numbers. This type look like this: \n''')
+int = '515'
+print(int)
+print(type(int))
+
+print('''\nThe more, let\'s call it "Advanced" version of "int" variable is Floating point data type,
+or "float" for short. In this type of variable we have a number with a fractional point. \
+In practice it look like this: \n''')
+float = 8.22
+print(float)
+print(type(float))
+
+
+
