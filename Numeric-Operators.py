5a8451373dfd85103080715a1aeda2952bdb602d
Vulpeslnculta <98029041+Vulpeslnculta@users.noreply.github.com>
2022-01-19 23:24:45 +0100

    Add files via upload
    Some data for numeric operators

---
+++ Numeric-Operators.py
@@ -0,0 +1,18 @@
+a = 10
+b = 3
+
+print(a + b) #13
+print(a - b) #7
+print(a * b) #30
+print(a / b) #3.33333...
+print(a // b) #3 integer division which is rounded down towards minus infinity
+print(a % b)  # 0 modulo: the remainder after integer division
+
+print()
+
+for i in range(1, a // b):
+    print(i)
+
+
+
+
