e93ce22472139ea889fcb07d1455051432bdeac2
Vulpeslnculta <98029041+Vulpeslnculta@users.noreply.github.com>
2022-01-19 22:07:46 +0100

    Add files via upload
    A little bit of variables fun with some interaction, and class changing

---
+++ Variables1.py
@@ -0,0 +1,16 @@
+greetings = ('Hi!')
+name = input('Enter your name here <3 : ')
+print(greetings + " " + name)
+age = 24
+print(age)
+print('age')
+print(type(age))
+print('name')
+print(type(name))
+print('greetings')
+print(type(greetings))
+# Now I will change 'age' value type from int to str:
+age = 'Twenty-Four'
+print('age')
+print(type(age))
+print('\nAs you can see ' + name + ', the type has changed from "int" to "str" as I promised!')
