f0ecb9483630fa88d7c570e57a6253e558ba4c9b
Nikodem Mucha <98029041+Vulpeslnculta@users.noreply.github.com>
2022-02-03 18:28:03 +0100

    Add files via upload

---
+++ set_diffrence.py
@@ -0,0 +1,19 @@
+from primes_and_squares import squares_generator, primes_generator
+
+evens = set(range(0, 50, 2))
+odds = set(range(1, 50, 2))
+
+print(evens, odds)
+
+primes = set(primes_generator(100))
+print(primes)
+squares = set(squares_generator(100))
+print(squares)
+
+print(odds.difference(primes))
+
+print(odds - primes)
+
+print(primes.difference(odds))
+
+print(primes - odds)
