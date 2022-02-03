32028de043fa82ef2e74812bbc56db043803939e
Nikodem Mucha <98029041+Vulpeslnculta@users.noreply.github.com>
2022-02-03 12:18:47 +0100

    Add files via upload

---
+++ set _intro.py
@@ -0,0 +1,11 @@
+farm_animals = {"cow", "sheep", "hen", "goat", "horse"}
+print(farm_animals)
+
+for animal in farm_animals:
+    print(animal)
+
+print()
+print("Indexing a sequence")
+animals_list = ["cow", "sheep", "hen", "goat", "horse"]
+goat = animals_list[3]
+print(goat)
