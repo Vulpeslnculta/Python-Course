994d54d87ed4ba88196c6db145799ce5aa7355da
Nikodem Mucha <98029041+Vulpeslnculta@users.noreply.github.com>
2022-02-06 15:01:05 +0100

    Add files via upload

--- tkindemo.py
+++ tkindemo.py
@@ -14,23 +14,32 @@ mainWindow.title("Testing")
 mainWindow.geometry('640x480+8+400')
 
 label = tkinter.Label(mainWindow, text="TESTING CANVAS")
-label.pack(side='top')
+label.grid(row=0, column=0)
 
 leftFrame = tkinter.Frame(mainWindow)
-leftFrame.pack(side='left', anchor='n', fill=tkinter.Y, expand=False)
-canvas = tkinter.Canvas(mainWindow, relief='raised', borderwidth=1)
-canvas.pack(side="top", anchor='n'
+leftFrame.grid(row=1, column=1)
+canvas = tkinter.Canvas(leftFrame, relief='raised', borderwidth=1)
+canvas.grid(row=1, column=0
             # fill=tkinter.BOTH, expand=True
             )
 rightFrame = tkinter.Frame(mainWindow)
-rightFrame.pack(side='right', anchor='n', expand=True)
+rightFrame.grid(row=1, column=2, sticky='n')
 
-button1 = tkinter.Button(mainWindow, text="Testing1")
-button2 = tkinter.Button(mainWindow, text="Testing2")
-button3 = tkinter.Button(mainWindow, text="Testing3")
-button1.pack(side='top')
-button2.pack(side='top')
-button3.pack(side='top')
+button1 = tkinter.Button(rightFrame, text="Testing1")
+button2 = tkinter.Button(rightFrame, text="Testing2")
+button3 = tkinter.Button(rightFrame, text="Testing3")
+button1.grid(row=0, column=0)
+button2.grid(row=1, column=0)
+button3.grid(row=2, column=0)
+
+mainWindow.columnconfigure(0, weight=1)
+mainWindow.columnconfigure(1, weight=1)
+mainWindow.grid_columnconfigure(2, weight=1)
+leftFrame.config(relief='sunken', borderwidth=1)
+rightFrame.config(relief='sunken', borderwidth=1)
+leftFrame.grid(sticky='ns')
+rightFrame.grid(sticky='ns')
+rightFrame.columnconfigure(0, weight=1)
 
 mainWindow.mainloop()
 
