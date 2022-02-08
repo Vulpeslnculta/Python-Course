1f00ae6a8cccf0e69eb61ae18746597592fcb543
Nikodem Mucha <98029041+Vulpeslnculta@users.noreply.github.com>
2022-02-08 16:05:03 +0100

    Add files via upload

--- morefunctions.py
+++ morefunctions.py
@@ -1,3 +1,5 @@
+import math
+
 try:
     import tkinter
 except ImportError:
@@ -7,7 +9,7 @@ except ImportError:
 def parabola(page, size):
     for x in range(-size, size):
         y = x * x / size
-        plot(canvas, x, -y)
+        plot(canvas, x, y)
 
 
 
@@ -21,8 +23,19 @@ def draw_Axes(canvas):
     print(locals())
 
 
-def plot(canvas, x, y):
-    canvas.create_line(x, y, x + 1, y + 1, fill='red')
+def plot(page, x, y):
+    canvas.create_line(x, -y, x + 1, -y + 1, fill='red')
+
+
+def circle(page, radius, g, h, color='red'):
+    # for x in range(g * 100, (g + radius) * 100):
+    #     x /= 100
+    #     y = h + (math.sqrt(radius**2 - ((x-g) ** 2)))
+    #     plot(page, x, y)
+    #     plot(page, x, 2 * h - y)
+    #     plot(page, 2 * g - x, y)
+    #     plot(page, 2 * g - x, 2 * h - y)
+    page.create_oval(g + radius, h + radius, g - radius, h - radius, outline=color, width=2)
 
 
 mainWindow = tkinter.Tk()
@@ -30,11 +43,11 @@ mainWindow = tkinter.Tk()
 mainWindow.title("Parabola")
 mainWindow.geometry('640x480')
 
-canvas = tkinter.Canvas(mainWindow, width=320, height=480)
+canvas = tkinter.Canvas(mainWindow, width=640, height=480)
 canvas.grid(row=0, column=0)
 
-canvas2 = tkinter.Canvas(mainWindow, width=320, height=480, background='violet')
-canvas2.grid(row=0, column=1)
+# canvas2 = tkinter.Canvas(mainWindow, width=320, height=480, background='violet')
+# canvas2.grid(row=0, column=1)
 
 
 draw_Axes(canvas)
@@ -42,4 +55,6 @@ draw_Axes(canvas)
 parabola(canvas, 100)
 parabola(canvas, 200)
 
+circle(canvas, 100, 100, 100, 'violet')
+circle(canvas, 100, -100, 100, 'cyan')
 mainWindow.mainloop()
