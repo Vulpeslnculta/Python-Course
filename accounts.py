67de332ba501634e9d5ecb9842119bfec9e510f3
Nikodem Mucha <98029041+Vulpeslnculta@users.noreply.github.com>
2022-02-09 19:04:01 +0100

    Add files via upload

--- accounts.py
+++ accounts.py
@@ -1,10 +1,22 @@
+import datetime
+import pytz
+
+
+
 class Account:
     """ Simple account with balance """
+
+    @staticmethod
+    def _current_time():
+        utc_time = datetime.datetime.utcnow()
+        return pytz.utc.localize(utc_time)
 
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
+        self.transaction_list = [(Account._current_time(), balance)]
         print("Account created for " + self.name)
+        self.show_balance()
 
     def deposit(self, amount):
         if amount > 0:
@@ -19,6 +31,14 @@ class Account:
     def show_balance(self):
         print("Balance is {}".format(self.balance))
 
+    def show_transactions(self):
+        for date, amount in self.trasaction_list:
+            if amount > 0:
+                tran_type = 'deposited'
+            else:
+                tran_type = 'withdrawn'
+            amount *= -1
+        print('{:6} {} on (local time was {})'.format(amount, tran_type, date, date.astimezone()))
 
 if __name__ == '__main__':
     niko = Account("Niko", 0)
@@ -26,3 +46,10 @@ if __name__ == '__main__':
 
     niko.deposit(10000)
     niko.show_balance()
+    niko.withdraw(500)
+    niko.show_balance()
+    niko.show_transactions()
+
+    Anne = Account("Anne", 515)
+    Anne.show_balance()
+    print(Anne.__dict__)
