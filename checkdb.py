0b0ff59f809f334dccee335d3c8c8c477372748e
Nikodem Mucha <98029041+Vulpeslnculta@users.noreply.github.com>
2022-02-18 14:17:14 +0100

    Add files via upload

--- checkdb.py
+++ checkdb.py
@@ -2,7 +2,13 @@ import sqlite3
 
 conn = sqlite3.connect("contacts.sqlite")
 
-for row in conn.execute("SELECT * FROM contacts"):
-    print(row)
+name = input("Please enter your name: \n")
+
+for name, phone, email in conn.execute("SELECT * FROM contacts WHERE name LIKE ?", (name,)):
+    print(name)
+    print(phone)
+    print(email)
+    print("_"*20)
+
 
 conn.close()
